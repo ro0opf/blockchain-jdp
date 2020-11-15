f235bb13967f0bafe91fe94b1ce897caa99ce85a
이종석(Lee Jong Suek)/플랫폼_Tech2그룹/SK <aristos2@sk.com>
2020-11-16 08:38:04 +0900

    corect code to deploy on ganache.

--- jdp-contract/migrations/2_deploy_contracts.js
+++ jdp-contract/migrations/2_deploy_contracts.js
@@ -1,14 +1,9 @@
-const ConvertLib = artifacts.require("ConvertLib");
-const MetaCoin = artifacts.require("Vote");
+const Context = artifacts.require("Context");
+const IERC20 = artifacts.require("IERC20");
+const SafeMath = artifacts.require("SafeMath");
+const ERC20 = artifacts.require("ERC20");
+const Vote = artifacts.require("Vote");
 
 module.exports = function(deployer) {
-  deployer.deploy(Context);
-  deployer.deploy(IERC20);
-  deployer.deploy(SafeMath);
-  deployer.deploy(ERC20);
-  deployer.link(Context, ERC20);
-  deployer.link(IERC20, ERC20);
-  deployer.link(SafeMath, ERC20);
-  deployer.link(ERC20, Vote);
   deployer.deploy(Vote);
 };
--- jdp-contract/test/sampleCallSmartContract.js
+++ jdp-contract/test/sampleCallSmartContract.js
@@ -1,21 +1,21 @@
 'use strict'
 const Web3 = require('web3');
 
-const provider = "http://127.0.0.1:7545";
+const provider = "http://127.0.0.1:8545";
 
 let web3 = new Web3(new Web3.providers.HttpProvider(provider));
 
 // get from remix 
-const JDPContractJson = require('./vote.json');
+const JDPContractJson = require('../build/contracts/Vote.json');
 const JDPABI = JDPContractJson.abi;
 // get from remix after deploy
-const ContractAddress = '0x42E2d19380cDB0daf2e4Db0d60A5EA41Ee971C68';
+const ContractAddress = '0x05f232F23369fa76021D95A8dFBfFB1d044C1012';
 
 const JDPContract = new web3.eth.Contract(JDPABI, ContractAddress);
 
 // get from ganache
-const address = '0xb06D97eB470f7a42967423bDbe34ACf03fDb3D06';
-const privateKey = '0x978102fedca401ae658f21f104af232ba81542559985d0a9c6910070994cae33';
+const address = '0xc0FEa6178d4A86EbB12D753D78F0Cb4860F93a34';
+const privateKey = '62639966e6e065f78554be0b065d705952ea41689a208769e4f772f697cd7f58';
 
 // set account 
 const account = web3.eth.accounts.privateKeyToAccount(privateKey);
@@ -27,7 +27,8 @@ const gasPrice = 20000000000;
 
 function callTransfer(recipient, amount) {
     return JDPContract.methods
-        .transfer(recipient , amount)
+        createEvent(0, ['0xabd9e3A669Aa5Fd54FAD41Fb19E0334d260Ad022','0x8ca761a9F36D6f87684fAd52410A4BB173d5b5dE' ])
+        //.transfer(recipient , amount)
         .send({
             from: address,
             gas: web3.utils.toHex(gasLimit),
@@ -46,7 +47,7 @@ function getBalance(addr){
     return JDPContract.methods.balanceOf(addr).call();
 }
 
-const addr = "0x7b1127bA63488934b38c9382d993017786fd53f2"
+const addr = "0xc0FEa6178d4A86EbB12D753D78F0Cb4860F93a34"
 const token = 100;
 callTransfer(addr, token);
 getBalance(addr).then(result => console.log(result));
