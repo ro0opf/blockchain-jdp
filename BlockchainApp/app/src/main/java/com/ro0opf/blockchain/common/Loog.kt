b541922e0b1568bf09e9f3e3b2f58db856e7a06e
ro0opf <34717634+ro0opf@users.noreply.github.com>
2020-11-17 03:09:17 +0900

    ..

---
+++ BlockchainApp/app/src/main/java/com/ro0opf/blockchain/common/Loog.kt
@@ -0,0 +1,11 @@
+package com.ro0opf.blockchain.common
+
+import android.util.Log
+
+object Loog {
+    val tag = "ro0opf"
+
+    fun e(msg: String) {
+        Log.e(tag, msg)
+    }
+}
--- BlockchainApp/app/src/main/java/com/ro0opf/blockchain/ui/account/AccountFragment.kt
+++ BlockchainApp/app/src/main/java/com/ro0opf/blockchain/ui/account/AccountFragment.kt
@@ -9,12 +9,14 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.ro0opf.blockchain.R
+import com.ro0opf.blockchain.common.Current
 import com.ro0opf.blockchain.databinding.FragmentAccountBinding
 import com.ro0opf.blockchain.ui.LoginActivity
+import com.ro0opf.blockchain.ui.home.HomeViewModel
 
 class AccountFragment : Fragment() {
 
-    private lateinit var accountViewModel: AccountViewModel
+    private lateinit var homeViewModel: HomeViewModel
     private lateinit var binding : FragmentAccountBinding
 
     override fun onCreateView(
@@ -22,22 +24,28 @@ class AccountFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        accountViewModel =
-            ViewModelProvider(this).get(AccountViewModel::class.java)
+        homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_account, container, false)
-//        val textView: TextView = root.findViewById(R.id.text_notifications)
-//        accountViewModel.text.observe(viewLifecycleOwner, Observer {
-//            textView.text = it
-//        })
 
+        setObserve()
         setOnClickListener()
+
+        homeViewModel.getBalance()
+
         return binding.root
+    }
+
+    private fun setObserve() {
+        binding.lifecycleOwner = this
+        binding.homeViewModel = homeViewModel
+        binding.user = Current.user
     }
 
     private fun setOnClickListener() {
         binding.btnLogout.setOnClickListener {
             startActivity(Intent(requireContext(), LoginActivity::class.java))
-            c
+            activity?.finish()
         }
     }
 }
